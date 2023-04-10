52106d7c928f14c24f80613caefa7794f557d5df
Kendarius James <kendariusjames18@gmail.com>
2023-04-10 16:20:26 -0500

    Changed the Test Cart Class
    8 to 7

--- netDeals/cart/tests.py
+++ netDeals/cart/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from cart.forms import CheckoutForm
+# Create your tests here.
+
+#This works
+class TestForms(TestCase):
+
+    def test_checkout_form_validate(self):
+        form = CheckoutForm(data={
+            'first_name': 'test',
+            'last_name': 'rest',
+            'email': 'a@gmail.com',
+            'address': 'test for best',
+            'phone': '6629986875',
+            'zipcode': '62847',
+            'place': 'apart',
+            'stripe_token': 'token'
+        }
+        )
+
+        self.assertTrue(form.is_valid())
+
+    def test_checkout_form_no_data(self):
+        form = CheckoutForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 7)
 
 # Create your tests here.
